0e2d5884faefc339d938b6ff0766c5c975f78f14
Md Anowar Hossain <anowarh761@gmail.com>
2019-06-26 23:40:36 +0600

    first commit

---
+++ myprogram.py
@@ -0,0 +1,13 @@
+h = 10
+e = 20
+l = 30
+o = 40
+print(h+e+l+l+o)
+
+gretting = input("Write here: ")
+print(gretting)
+
+age = input("Enter your age: ")
+father_age = float(age) + 25
+print(father_age)
+
